2dee06c8e099f61a8d600105e5351b3d95146ef9
350969 <350969@0338S-Z-90TDE0Q.sdcs.local>
2019-12-17 14:25:36 -0800

    String method, pi aloud, HappyPet(ip)

--- src/_08_string_methods/StringMethods.java
+++ src/_08_string_methods/StringMethods.java
@@ -19,16 +19,23 @@ public class StringMethods {
 
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-for(int i = 0; i< s.length();i++) {
-	System.out.println(s.charAt(i));
+		for(int i = 0; i< s.length();i++) {
+			System.out.println(s.charAt(i));
 }
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
-		//             for char 'b', then print "b is at index 1"
-		
+		//             for char 'b', then print "b is at index 1" 
+
+		for(int i = 0; i< s.length();i++) {
+				System.out.println(s.charAt(i));
+	if(s.charAt(i) == 'k') {
+		System.out.println(i);
+	}
+	
+}
 	}
 }
 
